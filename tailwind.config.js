dcf2dd500263fcd3bf91fd45222a75125b7dc19a
KIMDJ\vzmzm <azmzm@naver.com>
2025-02-05 23:18:13 +0900

    [0205] 집 커밋

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                roomi: '#9f6be7', // 프로젝트 메인 컬러 설정
+            },
+        },
+    },
+    plugins: [],
+};
