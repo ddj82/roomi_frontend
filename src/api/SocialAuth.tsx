6d563425ddd0487f10178b397788405b609a85f4
KIMDJ\vzmzm <azmzm@naver.com>
2025-03-26 10:57:23 +0900

    [dj][2025-03-26]

--- src/api/SocialAuth.tsx
+++ src/api/SocialAuth.tsx
@@ -53,6 +53,8 @@ export class SocialAuth {
     static async kakaoLogin() {
         try {
             const KAKAO_JS_KEY = "7e84a0bfbb21e40d283ad5d48d3d9d6c";
+            // const REDIRECT_URI = 'http://localhost:8081/sign-up'; //Redirect URI
+            const REDIRECT_URI = 'https://roomi.co.kr/sign-up'; //Redirect URI
 
             // Kakao SDK 초기화 (중복 방지)
             if (!window.Kakao) {
@@ -61,6 +63,10 @@ export class SocialAuth {
             if (!window.Kakao.isInitialized()) {
                 window.Kakao.init(KAKAO_JS_KEY);
             }
+
+            window.Kakao.Auth.authorize({
+                redirectUri: REDIRECT_URI,
+            });
 
         } catch (error: unknown) {
             return { success: false, error };
