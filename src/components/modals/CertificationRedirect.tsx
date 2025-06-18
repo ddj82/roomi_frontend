de60d58393900014ec01e152b900754bfc3d5b01
KIMDJ\vzmzm <azmzm@naver.com>
2025-06-18 11:16:32 +0900

    [dj][2025-06-18] 인증처리중

--- src/components/modals/CertificationRedirect.tsx
+++ src/components/modals/CertificationRedirect.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useTranslation} from "react-i18next";
-import {useNavigate} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 
 export default function CertificationRedirect() {
     const {t} = useTranslation();
     const navigate = useNavigate();
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const success = searchParams.get('success');
 
     const handleGoMyInfo = () => {
         navigate('/myPage/내%20정보');
         window.location.reload();
     };
 
+    useEffect(() => {
+        console.log('success값',success);
+    }, [searchParams]);
+
     return (
         <div>
             <div>
-                통합 인증 성공!
+                통합 인증 성공!, 모바일 일때만 옴, 성공처리 여기서
+                <div>{success}</div>
             </div>
             <button
                 type="button"
