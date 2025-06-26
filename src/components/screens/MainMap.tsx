12082c46f6651a1f118782e43d04137e807d29dd
KIMDJ\vzmzm <azmzm@naver.com>
2025-06-26 17:59:34 +0900

    [dj][2025-06-26] 작업

--- src/components/screens/MainMap.tsx
+++ src/components/screens/MainMap.tsx
@@ -5,6 +5,7 @@ import HomeScreen from "src/components/screens/HomeScreen";
 import {useTranslation} from "react-i18next";
 import GoogleMap from "../map/GoogleMap";
 import AccodionItem from "../util/AccodionItem";
+import GoogleMap22 from "../map/GoogleMap22";
 
 export default function MainMap({isMobile}: { isMobile: boolean; }) {
     const [rooms, setRooms] = useState<RoomData[]>([]);
@@ -30,13 +31,9 @@ export default function MainMap({isMobile}: { isMobile: boolean; }) {
                     <div className="relative w-full !h-[calc(100dvh-5rem)] overflow-hidden">
                         {/* 지도 영역 */}
                         <div
-                            className={`
-                                absolute inset-0 mb-[2.5rem]
-                                transition-transform duration-300
-                                ${mobileRoomListOpen ? 'translate-y-full' : 'translate-y-0'}
-                            `}
+                            className={`absolute inset-0 mb-[2.5rem] transition-transform duration-300 ${mobileRoomListOpen ? 'translate-y-full' : 'translate-y-0'}`}
                         >
-                            <GoogleMap onRoomsUpdate={handleRoomsUpdate}/>
+                            <GoogleMap22 onRoomsUpdate={handleRoomsUpdate}/>
                         </div>
 
                         {/* 리스트 영역 */}
@@ -67,7 +64,7 @@ export default function MainMap({isMobile}: { isMobile: boolean; }) {
                 <div className="flex !h-[calc(100vh-5rem)] w-full"> {/* 브라우저 */}
                     {/* 왼쪽 지도 영역 - 70% */}
                     <div className="w-[70%] h-full relative">
-                        <GoogleMap onRoomsUpdate={handleRoomsUpdate}/>
+                        <GoogleMap22 onRoomsUpdate={handleRoomsUpdate}/>
                     </div>
                     {/* 오른쪽 리스트 영역 - 30% */}
                     <div className="w-[30%] h-full border-l border-gray-200 overflow-y-auto pb-4 scrollbar-hidden">
